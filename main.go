a3230621702ca0c78dab20f04a8295a70ceffa7b
koushik0329 <kkoushik_cse1905e2@mgit.ac.in>
2024-09-04 17:39:04 -0500

    maps

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"booking-app/helper"
 	"fmt"
-	"strings"
+	"strconv"
 )
 
 	var conferenceName = "Go Conference"
 	const conferenceTickets int= 50
 	var remainingTickets uint= 50
-	var bookings = []string{}
+	var bookings = make([]map[string]string, 0)
 
 func main() {
 	
@@ -62,8 +62,8 @@ func greetUsers(){
 func getFirstNames() []string{
 	firstNames := []string{}
 		for _, booking := range bookings{
-			var names=strings.Fields(booking)
-			firstNames=append(firstNames, names[0])
+			
+			firstNames=append(firstNames, booking["firstName"])
 
 		}
 		return firstNames
@@ -95,8 +95,19 @@ func getUserInput() (string, string, string, uint){
 
 func bookTicket(userTickets uint,  firstName string, lastName string, email string){
 		remainingTickets=remainingTickets-userTickets
+
+
+
+		var userData = make(map[string]string)
+
+		userData["firstName"] = firstName
+		userData["lastName"] = lastName
+		userData["email"] = email 
+		userData["numberOfTickets"]=strconv.FormatUint(uint64(userTickets), 10)
+
 	
-		bookings = append(bookings, firstName+" "+lastName)
+		bookings = append(bookings, userData)
+		fmt.Println("List of bookings is %v\n", bookings)
 
 		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n",firstName,lastName,userTickets,email)
 
